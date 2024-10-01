f15a0e6e947db033b56a19fd0b6d73318e418507
Diallo Ibrahima <ibrahima.k.diallo@student.vinci.be>
2024-10-01 11:17:26 +0200

    new: exo 1.1

---
+++ exercices/exercice 1.1bis/types.ts
@@ -0,0 +1,34 @@
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+interface Drink {
+  id: number;
+  title: string;
+  image: string;
+  volume: number;
+  price: number;
+}
+
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+
+export type { Pizza, NewPizza, PizzaToUpdate, Drink, Film };
